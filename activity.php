38be1468516bced0ab9622e766ce85272d6a4a45
harris460 <44542937+harris460@users.noreply.github.com>
2018-12-26 04:53:50 -0800

    lecture 16 (php activity)

---
+++ activity.php
@@ -0,0 +1,11 @@
+<?php
+$arr=array(" clean the air.","slow water runoff","prevent soil erosion","cool our homes, streets, and cities "," help buffer noise pollution.","helps in reducing global warming ","decrease effect of sunlight ");
+for($i=0; $i <=6;$i++)
+{
+	echo "<li>";
+		echo $arr[$i];
+	echo "</li>";
+}
+
+
+?>
